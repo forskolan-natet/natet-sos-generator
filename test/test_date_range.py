bd9e9ca541f471142f175764288b50ed467598da
Andreas Cederström <andreas.cederstrom@gmail.com>
2019-11-09 16:27:57 +0100

    assertEquals is deprecated

--- test/test_date_range.py
+++ test/test_date_range.py
@@ -8,12 +8,12 @@ class TestDateRange(TestCase):
 
     def test_correct_range_is_generated_when_only_one_day(self):
         dates = list(date_range(date(2018, 12, 30), date(2018, 12, 31)))
-        self.assertEquals(len(dates), 1)
-        self.assertEquals(dates[0], date(2018, 12, 30))
+        self.assertEqual(len(dates), 1)
+        self.assertEqual(dates[0], date(2018, 12, 30))
 
     def test_correct_range_is_generated_when_multiple_days(self):
         dates = list(date_range(date(2018, 12, 28), date(2018, 12, 31)))
-        self.assertEquals(len(dates), 3)
-        self.assertEquals(dates[0], date(2018, 12, 28))
-        self.assertEquals(dates[1], date(2018, 12, 29))
-        self.assertEquals(dates[2], date(2018, 12, 30))
+        self.assertEqual(len(dates), 3)
+        self.assertEqual(dates[0], date(2018, 12, 28))
+        self.assertEqual(dates[1], date(2018, 12, 29))
+        self.assertEqual(dates[2], date(2018, 12, 30))
